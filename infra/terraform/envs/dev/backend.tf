c7093df40f2c9a6ebb168f3cb57007a9f0340783
Turan Babasoy <babasoyturan1@gmail.com>
2026-05-13 00:25:47 +0400

    Update Terraform backend state storage for recovery

--- infra/terraform/envs/dev/backend.tf
+++ infra/terraform/envs/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-g6-tfstate-plc"
-    storage_account_name = "stg6tfstateplc"
+    storage_account_name = "stg6tfstateplc60717"
     container_name       = "tfstate"
     key                  = "dev.tfstate"
     use_azuread_auth     = true
