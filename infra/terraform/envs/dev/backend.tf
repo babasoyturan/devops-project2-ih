e95d51721781c82b96770ef7876ee8b4ef6c44a3
Turan Babasoy <babasoyturan1@gmail.com>
2026-05-13 00:55:08 +0400

    Restore Terraform backend to existing remote state

--- infra/terraform/envs/dev/backend.tf
+++ infra/terraform/envs/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-g6-tfstate-plc"
-    storage_account_name = "stg6tfstateplc60717"
+    storage_account_name = "stg6tfstateplc"
     container_name       = "tfstate"
     key                  = "dev.tfstate"
     use_azuread_auth     = true
